a7865fd474bf9b39b98d18521e7c830d8e348a3f
zainazeem <61400299+zainazeem@users.noreply.github.com>
2022-11-14 20:32:16 -0500

    Cleanup

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,25 +1,38 @@
-import React, { useEffect } from 'react';
-
-import { useUser } from '@auth0/nextjs-auth0';
+import React, { useEffect, useState } from 'react';
 
 const Hero = () => {
-  const { user } = useUser();
+  const [authenticated, setAuthenticated] = useState(false);
+  const [userId, setUserId] = useState('');
 
   const handleQrLogin = async refresh_token => {
-    const res = await fetch('https://keyri.us.auth0.com/oauth/token', {
-      method: 'POST',
-      headers: {
-        'content-type': 'application/x-www-form-urlencoded'
-      },
-      body: new URLSearchParams({
-        grant_type: 'refresh_token',
-        client_id: 'K4RWxNFhsHU7xDTxVqGzW9x2JWlf5ily',
-        client_secret: '7RlMb6UVQrThv6bhnhi-ZlYPKH5to8UPstuMmCrefBsSCgnMwe2O_iDUxLX5gEIg',
-        refresh_token: refresh_token
-      })
-    });
-    const data = await res.json();
-    console.log(data);
+    try {
+      const res = await fetch('https://keyri.us.auth0.com/oauth/token', {
+        method: 'POST',
+        headers: {
+          'content-type': 'application/x-www-form-urlencoded'
+        },
+        body: new URLSearchParams({
+          grant_type: 'refresh_token',
+          client_id: 'K4RWxNFhsHU7xDTxVqGzW9x2JWlf5ily',
+          client_secret: '7RlMb6UVQrThv6bhnhi-ZlYPKH5to8UPstuMmCrefBsSCgnMwe2O_iDUxLX5gEIg',
+          refresh_token: refresh_token
+        })
+      });
+      const data = await res.json();
+
+      localStorage.setItem('access_token', data.access_token);
+      localStorage.setItem('id_token', data.id_token);
+
+      // Decode the JWT to get the user's name
+      const jwt = data.id_token.split('.')[1];
+      const decodedJwt = JSON.parse(atob(jwt));
+      setUserId(decodedJwt.sub);
+      setAuthenticated(true);
+
+      location.reload();
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   useEffect(() => {
@@ -42,10 +55,10 @@ const Hero = () => {
       <h1 className="mb-4" data-testid="hero-title">
         Auth0 + Keyri Sample Project
       </h1>
-      {!user && (
+      {!authenticated && (
         <div>
           <p className="lead" data-testid="hero-lead">
-            First, register with the button in the navbar, then log in with the QR code.
+            Log in by scanning the QR code below with the example Keyri-Auth0 app.
           </p>
           <iframe
             src="./KeyriQR.html"
@@ -57,10 +70,10 @@ const Hero = () => {
             style={{ borderWidth: 0 }}></iframe>
         </div>
       )}
-      {user && (
+      {authenticated && (
         <div>
           <p className="lead" data-testid="hero-lead">
-            Logged in! You can now access the profile page.
+            Logged in! Your userId is ${userId}
           </p>
         </div>
       )}
